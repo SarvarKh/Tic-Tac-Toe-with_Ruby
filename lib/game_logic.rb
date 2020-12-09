a66705eb9684b60af4b26beca7de75117efd7196
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-09 20:07:18 +0500

    Added board class to track moves

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,3 +1,18 @@
 class Board
-  
+  @@board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+  ]
+
+  def self.print_board(selected_cell, user_turn)
+    @@board[selected_cell - 1] = user_turn
+
+    puts ''
+    print @@board[0..2]
+    puts ''
+    print @@board[3..5]
+    puts ''
+    print @@board[6..8]
+  end
 end
