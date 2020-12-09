e44c35d8d97feeaef11b84f86d8bfe83d92758a0
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-09 23:47:24 +0500

    Fixed linter errors except attr_reader

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,26 +1,26 @@
 class Board
-  @@board = [
+  @board = [
     '', '', '',
     '', '', '',
     '', '', ''
   ]
 
   def self.print_board(selected_cell, user_turn)
-    @@board[selected_cell - 1] = user_turn
+    @board[selected_cell - 1] = user_turn
 
     puts ''
-    print @@board[0..2]
+    print @board[0..2]
     puts ''
-    print @@board[3..5]
+    print @board[3..5]
     puts ''
-    print @@board[6..8]
+    print @board[6..8]
   end
 end
 
 class InnerBoard
   # attr_accessor :board_for_x, :board_for_o
 
-  @@winning_combinations = [
+  @winning_combinations = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -31,23 +31,23 @@ class InnerBoard
     [3, 5, 7]
   ]
 
-  @@board_for_x = [
+  @board_for_x = [
     '', '', '',
     '', '', '',
     '', '', ''
   ]
 
-  @@board_for_o = [
+  @board_for_o = [
     '', '', '',
     '', '', '',
     '', '', ''
   ]
 
   def self.board_for_x
-    @@board_for_x
+    @board_for_x
   end
   def self.board_for_o
-    @@board_for_o
+    @board_for_o
   end
 
   def self.move_inner_board(move, board)
@@ -56,12 +56,12 @@ class InnerBoard
 
   def self.check_win_combo(board, user)
     i = 0
-    winner_print = "\n\n\n************************\n**                    **\n**    #{user} won!     **\n**                    **\n************************\n\n"
-    while i < @@winning_combinations.size
-      if @@winning_combinations[i].all? { |number| board.include? number }
-        puts winner_print 
+    winner_print = "\n\n\n*******************\n**               **\n**  #{user} won!   **\n**               **\n*******************\n\n"
+    while i < @winning_combinations.size
+      if @winning_combinations[i].all? { |number| board.include? number }
+        puts winner_print
         exit
-      end      
+      end
       i += 1
     end
   end
