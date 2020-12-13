df8ef9c0126b6ddfa85baa731c7fbd32e8dde2ef
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-13 23:46:18 +0500

    Refractored InnerBoard class to Game class partially

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,16 @@
 class Board
   attr_accessor :game_board
+
   def initialize
     @game_board = ['', '', '', '', '', '', '', '', '']
   end
+
   def move(user_move, user_sign)
     @user_move = user_move
     @user_sign = user_sign
     @game_board[@user_move.to_i - 1] = @user_sign
   end
+
   def display
     [" #{@game_board[0]}  | #{@game_board[1]}  | #{@game_board[2]} ",
      '------------',
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,21 +1,18 @@
-# class Board
-#   @board = [
-#     '', '', '',
-#     '', '', '',
-#     '', '', ''
-#   ]
+require_relative './board.rb'
 
-#   def self.print_board(selected_cell, user_turn)
-#     @board[selected_cell - 1] = user_turn
+class Game < Board
 
-#     puts ''
-#     print @board[0..2]
-#     puts ''
-#     print @board[3..5]
-#     puts ''
-#     print @board[6..8]
-#   end
-# end
+  WINNING_COMBINATIONS = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ]
+end
 
 # class InnerBoard
 #   attr_accessor :board_for_x, :board_for_o
