5ce65aa10dce94f4ac3b77044eb9d9452b8377ce
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-14 00:48:58 +0500

    Added checker taken for move in class Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,21 @@
 class Board
-  attr_accessor :game_board
+  attr_accessor :game_board, :turn_counter
 
   def initialize
     @game_board = ['', '', '', '', '', '', '', '', '']
+    @turn_counter = 0
   end
 
   def move(user_move, user_sign)
     @user_move = user_move
     @user_sign = user_sign
     @game_board[@user_move.to_i - 1] = @user_sign
+  end
+
+  def taken_move?(move, board)
+    @move = move
+    @board = board
+    board[move.to_i - 1] != ''
   end
 
   def display
