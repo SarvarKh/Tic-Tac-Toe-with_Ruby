f206e5fc6a7cee1f88f31eeff4cfab419d366020
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-13 21:01:58 +0500

    Refractored game rules section in main

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,3 @@
 class Player
-  def initialize(name)
-    @name = name
-  end
-
-  def name_valid?(name)
-    unless name == ''
-      puts 'Invalid name, enter another name'
-      @name = gets.chomp
-      name_valid?(@name)
-    end
-  end
+  attr_accessor :name
 end
