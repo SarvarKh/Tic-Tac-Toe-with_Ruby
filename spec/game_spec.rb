4a6f5330d78147b45c51ad2d5a26abaa381b37d2
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-25 14:25:02 +0500

    Add RSpec test cases for move  method of Board class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,25 +1,57 @@
 require_relative '../lib/board.rb'
 
 describe Board do
+  my_board = Board.new
+
   describe 'initialize' do
     it "returns initialized instance variable game_board " do
-      my_board = Board.new
       expect(my_board.game_board[2]).to eql(' ')
     end
 
     it "returns initialized instance variable turn_counter " do
-      my_board = Board.new
       expect(my_board.turn_counter).to eql(0)
     end
     
     it "returns initialized instance variables boardx " do
-      my_board = Board.new
       expect(my_board.boardx).to eql([])
     end
 
     it "returns initialized instance variables boardo " do
-      my_board = Board.new
       expect(my_board.boardo).to eql([])
+    end
+  end
+
+  describe 'move' do
+    it "adds user 'X' move to the game board " do
+      my_board.move(2, 'x', my_board.boardx)
+      expectation = my_board.game_board[1]
+      actual = 'x'
+      expect(expectation).to eql(actual)
+    end
+
+    it "adds user 'O' move to the game board " do
+      my_board.move(3, 'o', my_board.boardo)
+      expectation = my_board.game_board[2]
+      actual = 'o'
+      expect(expectation).to eql(actual)
+    end
+
+    it "adds user moves as an integer into the user_board " do
+      my_board.boardo = [] # To clear inputs made from aboe 2 methods
+      
+      my_board.move(1, 'o', my_board.boardo)
+      expectation = my_board.boardo
+      actual = [1]
+      expect(expectation).to eql(actual)
+    end
+
+    it "adds user moves as an integer into the user_board " do
+      my_board.boardx = [] # To clear inputs made from aboe 2 methods
+
+      my_board.move(6, 'x', my_board.boardx)
+      expectation = my_board.boardx
+      actual = [6]
+      expect(expectation).to eql(actual)
     end
   end
 end
